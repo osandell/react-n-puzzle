2cc656d74092c06a52f044287244020c33dc88f8
Olof Sandell <contact@olofsandell.com>
2021-08-20 10:19:46 +0200

    feat: add a first draft of the board

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders "react-n-puzzle"', () => {
+test('renders a board', () => {
   render(<App />)
-  const linkElement = screen.getByText(/react-n-puzzle/i)
+  const linkElement = screen.getByTestId('board')
   expect(linkElement).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { CssBaseline } from '@material-ui/core'
+
+// components
+import Tile from './components/Tile'
 
 function App() {
-  return <>react-n-puzzle</>
+  return (
+    <>
+      <CssBaseline />
+      <Tile value={12} />
+    </>
+  )
 }
 
 export default App
