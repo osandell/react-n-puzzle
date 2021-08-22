ead6628392ca63bed1a584edd78e613533ba2886
Olof Sandell <contact@olofsandell.com>
2021-08-22 11:31:26 +0200

    refactor(fonts): change the font to Open Sans
    re #4

--- src/components/Board/Tile/Tile.tsx
+++ src/components/Board/Tile/Tile.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactElement } from 'react'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
 
 // Interfaces
 import TilePosition from '../../../shared/interfaces/TilePosition.interface'
@@ -16,7 +17,15 @@ const useStyles = makeStyles<Theme, Props>((theme: Theme) =>
       width: 100,
       height: 100,
       margin: 5,
-      background: 'white',
+      backgroundColor: '#444',
+      '&:hover': {
+        backgroundColor: '#00000090',
+      },
+      '@media (hover: hover)': {
+        '&:hover': {
+          backgroundColor: '#00000090',
+        },
+      },
       // We always have one button with value === null that represents the empty square,
       // that's why we hide it.
       visibility: `${props.value === null ? 'hidden' : 'visible'}`,
@@ -28,13 +37,13 @@ const Tile: FC<Props> = (props): ReactElement => {
   const classes = useStyles(props)
 
   return (
-    <button
+    <Button
       className={classes.root}
       onClick={() => props.onClick(props.position)}
     >
       {/* We need some placeholder string even on the hidden button to keep the right dimensions. */}
       {props.value === null ? '\u00A0' : props.value}
-    </button>
+    </Button>
   )
 }
 
